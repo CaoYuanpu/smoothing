f13dde4e6c156aaeaf788aa9c745907b65f8e70a
CaoYuanpu <yuanpucao@gmail.com>
2023-09-23 16:42:46 -0400

    dev

--- code/pgd_l2.py
+++ code/pgd_l2.py
@@ -36,9 +36,9 @@ if __name__ == "__main__":
 
         (x, label) = dataset[i]
         x = x.cuda()
-        # label = label.cuda()
+        label = torch.tensor(label, dtype=torch.int64).cuda()
         batch = x.repeat((1, 1, 1, 1))
-        label = [label]
+        label = label.repeat((1, 1))
         
         predictions = base_classifier(batch).argmax(1)
         print(predictions, label, predictions[0]==label[0])
