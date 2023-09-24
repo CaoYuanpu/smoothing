c5b2caea63b6e75f9ae1f4c00902b6088f4bd25c
CaoYuanpu <yuanpucao@gmail.com>
2023-09-24 17:49:52 -0400

    dev

--- code/certify_failure.py
+++ code/certify_failure.py
@@ -61,8 +61,8 @@ if __name__ == "__main__":
 
     
     # prepare output file
-    # f = open(args.outfile, 'w')
-    # print("idx\tlabel\tpredict\tradius\tnA\tpAPrime\tpABar\tcorrect\ttime\tdistance\tbudget", file=f, flush=True)
+    f = open(args.outfile, 'w')
+    print("idx\tlabel\tpredict\tradius\tnA\tpAPrime\tpABar\tcorrect\ttime\tdistance\tbudget", file=f, flush=True)
 
     # iterate through the dataset
     dataset = get_dataset(args.dataset, args.split)
@@ -90,28 +90,30 @@ if __name__ == "__main__":
 
             x_ = x.repeat((1, 1, 1, 1))
             cAHat = smoothed_classifier.guess(x_.cuda(), args.N0, args.batch)
-            print(label, cAHat)
-            input()
-        #     target = torch.tensor(label, dtype=torch.int64)
-        #     target = target.repeat((1))
-        #     # atk = attack.EOTPGDL2(boost_classifier, eps=1.0, alpha=0.1, steps=10, eot_iter=10)
-        #     atk = attack.EOTPGDL2(boost_classifier, eps=radius, alpha=radius*2/10, steps=10, eot_iter=100)
-        #     atk.set_mode_targeted_by_function(target_map_function=lambda images, labels:labels)
-        #     x_adv = atk(x_.cuda(), target.cuda())
-            
+            if label == cAHat:
+                target = torch.tensor(label, dtype=torch.int64)
+                target = target.repeat((1))
+                # atk = attack.EOTPGDL2(boost_classifier, eps=1.0, alpha=0.1, steps=10, eot_iter=10)
+                radius = 0.5
+                atk = attack.EOTPGDL2(boost_classifier, eps=radius, alpha=radius*2/10, steps=10, eot_iter=100)
+                atk.set_mode_targeted_by_function(target_map_function=lambda images, labels:labels)
+                x_adv = atk(x_.cuda(), target.cuda())
+                
 
-        #     # certify the prediction of g around x_adv
-        #     prediction_adv, radius_adv, nA, pAPrime, pABar = smoothed_classifier.certify(x_adv, args.N0, args.N, args.alpha, args.batch)
-        #     after_time = time()
-        #     correct = int(prediction_adv == label)
+                # certify the prediction of g around x_adv
+                prediction_adv, radius_adv, nA, pAPrime, pABar = smoothed_classifier.certify(x_adv, args.N0, args.N, args.alpha, args.batch)
+                after_time = time()
+                correct = int(prediction_adv == label)
 
-        #     time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
-        #     distance = torch.linalg.norm((x_adv - x_.cuda()).detach()[0]).cpu().numpy()
-        #     budget = radius_adv - radius
-            
-        #     print("{}\t{}\t{}\t{:.3}\t{}\t{:.3}\t{:.3}\t{}\t{}\t{:.3}\t{:.3}".format(
-        #         i, label, prediction_adv, radius_adv, nA, pAPrime, pABar, correct, time_elapsed, distance, budget), file=f, flush=True)
-        # else:
-        #     print('\t'.join(res)[:-1], file=f, flush=True)
+                time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
+                distance = torch.linalg.norm((x_adv - x_.cuda()).detach()[0]).cpu().numpy()
+                budget = radius_adv - radius
+                
+                print("{}\t{}\t{}\t{:.3}\t{}\t{:.3}\t{:.3}\t{}\t{}\t{:.3}\t{:.3}".format(
+                    i, label, prediction_adv, radius_adv, nA, pAPrime, pABar, correct, time_elapsed, distance, budget), file=f, flush=True)
+            else:
+                print('\t'.join(res)[:-1], file=f, flush=True)
+        else:
+            print('\t'.join(res)[:-1], file=f, flush=True)
 
     f.close()
