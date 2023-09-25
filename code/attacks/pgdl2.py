cca98c8d201c9249ec3567ac48381a8b94651908
CaoYuanpu <yuanpucao@gmail.com>
2023-09-25 16:22:40 -0400

    dev

--- code/attacks/pgdl2.py
+++ code/attacks/pgdl2.py
@@ -262,8 +262,9 @@ class NegtiveEOTPGDL2(Attack):
         for _ in range(self.steps):
             print(adv_images.shape)
             print(target_labels)
+            print(self.model.sigma)
             input()
-            negtive_noises = self.get_negative_samples(adv_images[0].clone(), target_labels[0])
+            negtive_noises = self.get_negative_samples(adv_images[0].clone(), target_labels[0], sigma=self.model.sigma)
             
             grad = torch.zeros_like(adv_images)
             adv_images.requires_grad = True
